57fb5d9b85481863fc696a41cb5d53e46ef60cdd
Bob Whitelock <bob.whitelock1@gmail.com>
2018-10-17 15:56:48 +0100

    Promote useful generated Gem files
    Promote the files we want to retain when converting all of Underware
    into a Gem, after running `bundle gem underware` (and then tweaking some
    things).

--- underware/spec/underware_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Underware do
-  it "has a version number" do
-    expect(Underware::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end

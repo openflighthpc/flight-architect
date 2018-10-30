c48ad10c3c801d3269184c1134adc7c7ed5423f7
Bob Whitelock <bob.whitelock1@gmail.com>
2018-10-30 12:51:04 +0000

    Delete method which has been moved to Metalware

--- lib/underware/spec/alces_utils.rb
+++ lib/underware/spec/alces_utils.rb
@@ -90,15 +90,6 @@ module Underware
         $stderr = hash[:stderr] if hash[:stderr]
       end
 
-      # XXX This is only used in Metalware, so maybe it should live there now
-      # instead?
-      def kill_other_threads
-        Thread.list
-              .reject { |t| t == Thread.current }
-              .tap { |t| t.each(&:kill) }
-              .tap { |t| t.each(&:join) }
-      end
-
       def mock(test, *a, &b)
         mock_block = lambda do |*_inputs|
           mock_alces = AlcesUtils::Mock.new(self)

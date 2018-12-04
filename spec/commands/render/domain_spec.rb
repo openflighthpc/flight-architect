db6ee5509de1f9be0f0dabda76cdd489b0b61a2f
Bob Whitelock <bob.whitelock1@gmail.com>
2018-12-04 16:11:12 +0000

    Support `--platform` option for `render` commands (closes #24)

--- spec/commands/render/domain_spec.rb
+++ spec/commands/render/domain_spec.rb
@@ -2,10 +2,10 @@
 require 'underware/commands/render/domain'
 
 RSpec.describe Underware::Commands::Render::Domain do
-  def run_command(template_path)
+  def run_command(*args)
     Underware::AlcesUtils.redirect_std(:stdout) do
       Underware::Utils.run_command(
-        Underware::Commands::Render::Domain, template_path
+        Underware::Commands::Render::Domain, *args
       )
     end[:stdout].read
   end
@@ -13,6 +13,7 @@ RSpec.describe Underware::Commands::Render::Domain do
   let :template do
     template_contents = <<~TEMPLATE.strip_heredoc
       Rendered with scope: <%= scope.class %>
+      Platform config value: <%= config.platform_config_key %>
     TEMPLATE
 
     Tempfile.create.tap { |t| t.write(template_contents) }
@@ -21,6 +22,33 @@ RSpec.describe Underware::Commands::Render::Domain do
   it 'renders template against the domain and outputs result' do
     output = run_command(template.path)
 
-    expect(output).to eq "Rendered with scope: Underware::Namespaces::Domain\n"
+    expect(output).to include "Rendered with scope: Underware::Namespaces::Domain\n"
+  end
+
+  describe 'with `--platform` option passed' do
+    let :platform_config_path do
+      Underware::FilePath.platform_config(:test_platform)
+    end
+
+    it 'includes config for given platform when forming namespace' do
+      Underware::Data.dump(
+        platform_config_path,
+        platform_config_key: 'platform_config_value'
+      )
+      File.read Underware::FilePath.platform_config(:test_platform)
+
+      output = run_command(template.path, platform: :test_platform)
+
+      expect(output).to include "Platform config value: platform_config_value\n"
+    end
+
+    it 'gives error if no config exists for platform' do
+      expect do
+        run_command(template.path, platform: :test_platform)
+      end.to raise_error(
+        Underware::InvalidInput,
+        "Unknown platform: test_platform (#{platform_config_path} does not exist)"
+      )
+    end
   end
 end
--- spec/commands/render/group_spec.rb
+++ spec/commands/render/group_spec.rb
@@ -22,6 +22,7 @@ RSpec.describe Underware::Commands::Render::Group do
     template_contents = <<~TEMPLATE.strip_heredoc
       Rendered with scope: <%= scope.class %>
       Scope name: <%= scope.name %>
+      Platform config value: <%= config.platform_config_key %>
     TEMPLATE
 
     Tempfile.create.tap { |t| t.write(template_contents) }
@@ -40,5 +41,32 @@ RSpec.describe Underware::Commands::Render::Group do
     end.to raise_error(
       Underware::InvalidInput, "Could not find group: unknown_group01"
     )
+  end
+
+  describe 'with `--platform` option passed' do
+    let :platform_config_path do
+      Underware::FilePath.platform_config(:test_platform)
+    end
+
+    it 'includes config for given platform when forming namespace' do
+      Underware::Data.dump(
+        platform_config_path,
+        platform_config_key: 'platform_config_value'
+      )
+      File.read Underware::FilePath.platform_config(:test_platform)
+
+      output = run_command(test_group_name, template.path, platform: :test_platform)
+
+      expect(output).to include "Platform config value: platform_config_value\n"
+    end
+
+    it 'gives error if no config exists for platform' do
+      expect do
+        run_command(test_group_name, template.path, platform: :test_platform)
+      end.to raise_error(
+        Underware::InvalidInput,
+        "Unknown platform: test_platform (#{platform_config_path} does not exist)"
+      )
+    end
   end
 end

75a5e50a2dc892dd14d6acf41f05eaf6c26a80f5
william <william.mccumstie@outlook.com>
2019-01-04 17:15:53 +0000

    Create the base files for the init command
    This command will eventually be used to create a basic cluster from
    nothing

---
+++ lib/underware/commands/init.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+#==============================================================================
+#
+# Copyright (C) 2019 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Underware.
+#
+# Alces Underware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Underware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on the Alces Underware, please visit:
+# https://github.com/alces-software/underware
+#==============================================================================
+
+module Underware
+  module Commands
+    class Init < CommandHelpers::BaseCommand
+      private
+
+      def setup; end
+
+      def run; end
+    end
+  end
+end

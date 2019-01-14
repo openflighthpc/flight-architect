f9eb68987f6691045aeb431a04b56f502d601a60
william <william.mccumstie@outlook.com>
2019-01-14 15:45:29 +0000

    Wrap the current cluster within a Config class
    This will need to become more intelligent at some point. For the time
    being, the `CLUSTER_IDENTIFIER` constant is being wrapped. This will
    stop the rest of the code base becoming coupled to it

--- lib/underware/config.rb
+++ lib/underware/config.rb
@@ -8,3 +8,11 @@
 ActiveSupport::Inflector.inflections do |inflect|
   inflect.irregular 'chassis', 'chassis'
 end
+
+module Underware
+  class Config
+    def self.current_cluster
+      Underware::Commands::Init::CLUSTER_IDENTIFIER
+    end
+  end
+end

65d3d09c1b2375b5b2810cd1423b6fc012e1212e
william <william.mccumstie@outlook.com>
2019-01-14 15:22:32 +0000

    Create a method for copying data files
    This is a clone of the `create_cluster` method within the `init`
    command. The main difference is this class has now been tested. This
    class will be used in the specs as well

---
+++ lib/underware/data_copy.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+#==============================================================================
+# Copyright (C) 2019 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Underware.
+#
+# Alces Underware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Underware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on the Alces Underware, please visit:
+# https://github.com/alces-software/underware
+#==============================================================================
+
+require 'underware/data_path'
+
+module Underware
+  class DataCopy
+    def initialize(source_cluster, destination_cluster)
+      @source = DataPath.new(cluster: source_cluster)
+      @destination = DataPath.new(cluster: destination_cluster)
+    end
+
+    def all
+      FileUtils.mkdir_p(destination.base)
+      Dir.glob(source.relative('*')) do |source_path|
+        relative_path = File.basename(source_path)
+        destination_path = destination.relative(relative_path)
+        FileUtils.copy_entry source_path, destination_path
+      end
+    end
+
+    private
+
+    attr_reader :source, :destination
+  end
+end
---
+++ spec/underware/data_copy_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+#==============================================================================
+# Copyright (C) 2019 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Underware.
+#
+# Alces Underware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Underware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on the Alces Underware, please visit:
+# https://github.com/alces-software/underware
+#==============================================================================
+
+require 'underware/data_copy'
+require 'pathname'
+
+RSpec.describe Underware::DataCopy do
+  shared_context 'with existing cluster1 files' do
+    let(:cluster1_path) { Underware::DataPath.new(cluster: 'cluster1') }
+    let(:cluster1_files) do
+      [
+        'file1',
+        ['directory', 'file2'],
+        ['directory', 'sub-directory', 'file3']
+      ].map { |p| cluster1_path.relative(*p) }
+    end
+
+    before do
+      cluster1_files.each do |path|
+        FileUtils.mkdir_p(File.dirname(path))
+        FileUtils.touch(path)
+      end
+    end
+  end
+
+  shared_context 'with a non-existant new cluster' do
+    let(:new_cluster) { 'new-cluster' }
+    let(:new_cluster_path) { Underware::DataPath.new(cluster: new_cluster) }
+  end
+
+  context 'when copying to a non existant cluster' do
+    include_context 'with existing cluster1 files'
+    include_context 'with a non-existant new cluster'
+
+    let(:new_paths) do
+      base = Pathname.new(cluster1_path.base)
+      cluster1_files.map { |p| Pathname.new(p).relative_path_from(base).to_s }
+                    .map { |p| File.expand_path(p, new_cluster_path.base) }
+    end
+    subject { described_class.new('cluster1', new_cluster) }
+
+    describe '#all' do
+      before { subject.all }
+
+      it 'copyies all the files into the new clusters directory' do
+        new_paths.each { |p| expect(Pathname.new(p)).to be_exist }
+      end
+    end
+  end
+end

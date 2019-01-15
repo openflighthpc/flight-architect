e1a07b61a405c5d9b18accf13ab21ec1f7108259
william <william.mccumstie@outlook.com>
2019-01-15 16:03:19 +0000

    Use the current cluster in the configure commands
    Otherwise they would always configure the `something` cluster

--- lib/underware/commands/configure/node.rb
+++ lib/underware/commands/configure/node.rb
@@ -1,5 +1,26 @@
-
 # frozen_string_literal: true
+
+#==============================================================================
+# Copyright (C) 2019 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Underware.
+#
+# Alces Underware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Underware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on the Alces Underware, please visit:
+# https://github.com/alces-software/underware
+#==============================================================================
 
 require 'underware/command_helpers/configure_command'
 require 'underware/constants'
@@ -27,7 +48,9 @@ module Underware
 
         def custom_configuration
           return if alces.nodes.find_by_name(node_name)
-          ClusterAttr.update('something') { |a| a.add_nodes(node_name) }
+          ClusterAttr.update(Underware::Config.current_cluster) do |attr|
+            attr.add_nodes(node_name)
+          end
         end
       end
     end

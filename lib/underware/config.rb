195e3aad54699f6661ab663b7b20b147805420c6
william <william.mccumstie@outlook.com>
2019-01-16 12:33:12 +0000

    Create an instance of the `Config` object
    The `Config` is a quasi global class as it dictates how the process
    runs. However there is a need to update the `Config` with CLI options
    and as such needs to be initialized

--- lib/underware/config.rb
+++ lib/underware/config.rb
@@ -1,8 +1,32 @@
 # frozen_string_literal: true
 
+#==============================================================================
+# Copyright (C) 2019 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Underware.
+#
+# Alces Underware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Underware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on the Alces Underware, please visit:
+# https://github.com/alces-software/underware
+#==============================================================================
+
 #
 # This file contains ruby configuration that should be constant
 #
+
+require 'active_support/core_ext/module/delegation'
 
 # Make 'chassis' both the singular and plural
 ActiveSupport::Inflector.inflections do |inflect|
@@ -11,7 +35,17 @@ end
 
 module Underware
   class Config
-    def self.current_cluster
+    class << self
+      def cache
+        @cache ||= new
+      end
+      delegate_missing_to :cache
+    end
+
+    def initialize
+    end
+
+    def current_cluster
       'cluster'
     end
   end

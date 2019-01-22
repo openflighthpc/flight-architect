4b2337709ba62dd9eeb2c032b956ad5d639a6518
william <william.mccumstie@outlook.com>
2019-01-22 13:22:24 +0000

    Suppress the `--trace` output on all errors
    This means the user will no longer see the `--trace` ever. It will also
    means `Underware::UserError` can be removed

--- lib/underware/cli.rb
+++ lib/underware/cli.rb
@@ -52,7 +52,8 @@ module Underware
         under-lying Alces clusters and other Alces tools
       EOF
 
-      suppress_trace_class UserUnderwareError
+      # Suppress the `--trace` on all outputs. It is now a semi-hidden feature
+      suppress_trace_class Exception
 
       CliHelper::Parser.new(self).parse_commands
 

1c02b42942891072ecf31dba3c65458b4d425818
william <william.mccumstie@outlook.com>
2019-01-22 11:07:46 +0000

    Remove the domain level `keys` method
    These methods are now broken as underware doesn't manage keys anymore

--- lib/underware/namespaces/domain.rb
+++ lib/underware/namespaces/domain.rb
@@ -16,17 +16,10 @@ module Underware
         @genders_url ||= DeploymentServer.system_file_url('genders')
       end
 
-      def keys
-        KeyPair.new(
-          File.read(FilePath.public_key),
-          File.read(FilePath.private_key)
-        )
-      end
-
       private
 
       def white_list_for_hasher
-        super.concat [:hostip, :hosts_url, :genders_url, :keys]
+        super.concat [:hostip, :hosts_url, :genders_url]
       end
 
       def additional_dynamic_namespace
--- spec/underware/namespaces/domain_spec.rb
+++ spec/underware/namespaces/domain_spec.rb
@@ -13,18 +13,6 @@ RSpec.describe Underware::Namespaces::Domain do
 
     include_examples Underware::Namespaces::HashMergerNamespace
 
-    before :each do
-      # Create key pair files here (in production created on install), both so
-      # can test in `#keys` (below) and so `HashMergerNamespace#to_h` tests
-      # pass as key values will be included in hashed domain.
-      Underware::Utils.create_file(
-        Underware::FilePath.private_key, content: 'my_private_key'
-      )
-      Underware::Utils.create_file(
-        Underware::FilePath.public_key, content: 'my_public_key'
-      )
-    end
-
     context 'with mocked ip' do
       let(:ip) { '1.2.3.4' }
 
@@ -44,26 +32,6 @@ RSpec.describe Underware::Namespaces::Domain do
       it 'has a genders_url' do
         url = "http://#{ip}/metalware/system/genders"
         expect(subject.genders_url).to eq(url)
-      end
-    end
-
-    describe '#keys' do
-      describe '#private' do
-        it 'provides access to private key from file' do
-
-          expect(subject.keys.private).to eq('my_private_key')
-        end
-      end
-
-      describe '#public' do
-        it 'provides access to public key from file' do
-
-          expect(subject.keys.public).to eq('my_public_key')
-        end
-      end
-
-      it 'errors if attempt to access any other properties' do
-        expect { subject.keys.foo }.to raise_error(NoMethodError)
       end
     end
   end

3b41c07b4796681bb621ac597359854df32f57f1
william <william.mccumstie@outlook.com>
2019-01-23 15:02:45 +0000

    Do not try and auto hash yourself
    The `WhilteListHasher` can not auto-hash other `WhilteListHasher`
    objects. This creates a infinite loop of auto_hashing that fails
    miserable

--- lib/underware/namespaces/mixins/white_list_hasher.rb
+++ lib/underware/namespaces/mixins/white_list_hasher.rb
@@ -1,54 +1,46 @@
 
 # frozen_string_literal: true
 
+#==============================================================================
+# Copyright (C) 2019 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Underware.
+#
+# Alces Underware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Underware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on the Alces Underware, please visit:
+# https://github.com/alces-software/underware
+#==============================================================================
+
 module Underware
   module Namespaces
     module Mixins
       module WhiteListHasher
-        def self.included(base)
-          base.extend(ClassMethods)
-        end
-
         def self.convert_to_hash(obj)
           if obj.is_a?(Array)
             obj.map { |o| convert_to_hash(o) }
-          elsif obj.respond_to?(:to_h)
+          elsif obj.respond_to?(:to_h) && !obj.is_a?(self)
             obj.to_h
           else
             obj
           end
         end
 
-        module ClassMethods
-          def method_added(name)
-            super
-
-            # Register hashable methods
-            return if hasher_skip_method(reset: true)
-            return if name == :initialize
-            return unless public_method_defined?(name)
-            if instance_method(name).arity == 0
-              hashable_methods << name.to_sym
-            end
-          end
-
-          def hashable_methods
-            @hashable_methods ||= []
-          end
-
-          def hasher_skip_method(reset: false)
-            if reset
-              value = @hasher_skip_method
-              @hasher_skip_method = false
-              return value
-            else
-              @hasher_skip_method = true
-            end
-          end
-        end
-
         def to_h
-          self.class.hashable_methods.reduce({}) do |memo, method|
+          (public_methods - Object.public_methods - [:to_h])
+            .select { |n| method(n).arity == 0 }
+            .reduce({}) do |memo, method|
             value = __send__(method)
             hashed_value = WhiteListHasher.convert_to_hash(value)
             memo.merge(method => hashed_value)
--- spec/underware/namespaces/mixins/white_list_hasher_spec.rb
+++ spec/underware/namespaces/mixins/white_list_hasher_spec.rb
@@ -1,6 +1,28 @@
 
 # frozen_string_literal: true
 
+#==============================================================================
+# Copyright (C) 2019 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Underware.
+#
+# Alces Underware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Underware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on the Alces Underware, please visit:
+# https://github.com/alces-software/underware
+#==============================================================================
+
 require 'underware/namespaces/mixins/white_list_hasher'
 require 'ostruct'
 
@@ -9,11 +31,6 @@ RSpec.describe Underware::Namespaces::Mixins::WhiteListHasher do
     ctx = __binding__
     Class.new do
       include ctx.eval('described_class')
-
-      hasher_skip_method
-      def do_not_hash_me
-        'ohh snap'
-      end
 
       ctx.eval('white_list').each do |method|
         define_method(method) { "#{method} - return" }

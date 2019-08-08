b715cb0a984228c5bf567cba1afff7e57db7aae3
stuts <stu.j.franks@gmail.com>
2019-08-08 11:50:13 +0100

    OpenFlight branded Flight Direct

--- data/example/lib/templates/core/node/plugins/flightdirect.bash
+++ data/example/lib/templates/core/node/plugins/flightdirect.bash
@@ -31,5 +31,9 @@ flight session enable base/gnome
 # Disable user gridware
 sed -i 's/.*cw_GRIDWARE_allow_users=.*/cw_GRIDWARE_allow_users=false/g' /opt/flight-direct/etc/gridware.rc
 
+# Add OpenFlight branding
+curl https://openflighthpc-compute.s3.eu-west-2.amazonaws.com/banner/openflight.sh > /opt/flight-direct/scripts/openflight.sh
+sed -i 's,scripts/moosebird,scripts/openflight,g' /opt/flight-direct/etc/profile.d/01-banner.sh
+
 # Enable storage types
 flight storage enable base/s3

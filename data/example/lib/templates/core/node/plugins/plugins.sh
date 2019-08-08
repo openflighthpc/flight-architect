be8bb34671d188e15b8e5230c913d3696d8e32fb
stuts <stu.j.franks@gmail.com>
2019-08-08 11:52:14 +0100

    Don't use Manage for plugins for now

--- data/example/lib/templates/core/node/plugins/plugins.sh
+++ data/example/lib/templates/core/node/plugins/plugins.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Install Flight Manage
-yum -y install flight-manage
+#yum -y install flight-manage
 
 # Create scripts directory
-NODEDIR=/opt/service/nodescripts/<%= node.name %>scripts
-mkdir -p $NODEDIR
-mkdir -p /opt/service/flight/managedata
+#NODEDIR=/opt/service/nodescripts/<%= node.name %>scripts
+#mkdir -p $NODEDIR
+#mkdir -p /opt/service/flight/managedata
 
 # Download scripts
-curl <%= config.renderedurl %>/core/plugins/flightdirect.bash > $NODEDIR/flightdirect.bash 
-curl <%= config.renderedurl %>/core/plugins/nfs.bash > $NODEDIR/nfs.bash 
-curl <%= config.renderedurl %>/core/plugins/slurm.bash > $NODEDIR/slurm.bash 
+curl <%= config.renderedurl %>/core/plugins/nfs.bash | /bin/bash -x
+curl <%= config.renderedurl %>/core/plugins/flightdirect.bash | /bin/bash -x
+curl <%= config.renderedurl %>/core/plugins/slurm.bash | /bin/bash -x
 
 # Set executable
-chmod +x -R $NODEDIR/*.bash
+#chmod +x -R $NODEDIR/*.bash

abfcac86aafcc9f2ce1fd39e3c7620aadac7a10b
stuts <stu.j.franks@gmail.com>
2019-08-27 11:34:00 +0100

    Include localhost lines in hosts

--- data/base/lib/templates/core/node/networking/hosts.sh
+++ data/base/lib/templates/core/node/networking/hosts.sh
@@ -1,4 +1,6 @@
 cat << EOF > /etc/hosts
+127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
+::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
 
 <% nodes.each do |node| -%>
 <% node.config.networks.each do |name, network| -%>

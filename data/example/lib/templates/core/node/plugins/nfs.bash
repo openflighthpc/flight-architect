d5e1f2dcf74f2531114f668228baf60d744fc224
stuts <stu.j.franks@gmail.com>
2019-09-09 11:44:01 +0100

    Remove gridware mount, add apps mount

--- data/example/lib/templates/core/node/plugins/nfs.bash
+++ data/example/lib/templates/core/node/plugins/nfs.bash
@@ -6,8 +6,8 @@ yum -y install nfs-utils
 
 <% if (node.config.gateway rescue false) -%>
 # Create export directories
+mkdir -p /export/apps
 mkdir -p /export/data
-mkdir -p /export/gridware
 mkdir -p /export/service
 mkdir -p /export/site
 
@@ -17,8 +17,8 @@ sed -ie "s/^#\RPCNFSDCOUNT.*$/\RPCNFSDCOUNT=32/g" /etc/sysconfig/nfs
 EXPORTOPTS="<%= config.networks.network1.network %>/<%= config.networks.network1.netmask %>(rw,no_root_squash,sync)"
 
 EXPORTS=`cat << EOF
+/export/apps "$EXPORTOPTS"
 /export/data "$EXPORTOPTS"
-/export/gridware "$EXPORTOPTS"
 /export/service "$EXPORTOPTS"
 /export/site "$EXPORTOPTS"
 /home "$EXPORTOPTS"
@@ -31,8 +31,8 @@ firewall-cmd --reload
 <% end -%>
 
 MOUNTS=`cat << EOF
+gateway1:/export/apps   /opt/apps   nfs     intr,rsize=32768,wsize=32768,vers=3,_netdev     0 0
 gateway1:/export/data   /data   nfs     intr,rsize=32768,wsize=32768,vers=3,_netdev     0 0
-gateway1:/export/gridware   /opt/gridware   nfs     intr,rsize=32768,wsize=32768,vers=3,_netdev     0 0
 gateway1:/export/service   /opt/service   nfs     intr,rsize=32768,wsize=32768,vers=3,_netdev     0 0
 gateway1:/export/site   /opt/site   nfs     intr,rsize=32768,wsize=32768,vers=3,_netdev     0 0
 <% unless (node.config.gateway rescue false) -%>
@@ -42,7 +42,7 @@ EOF`
 
 echo "$MOUNTS" >> /etc/fstab
 
-mkdir -p /data /opt/gridware /opt/service /opt/site 
+mkdir -p /data /opt/apps /opt/service /opt/site
 
 
 systemctl enable nfs
